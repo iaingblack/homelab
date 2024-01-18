dff961344e85bc3eb6a4822de9ec9921115b4ee4
iain <iain@rootisgod.com>
2024-01-18 07:27:35 +0000

    Weird character in install command

--- kubernetes/server/trig-k3s/install-k3s.sh
+++ kubernetes/server/trig-k3s/install-k3s.sh
@@ -1,2 +1,1 @@
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --disable traefik"  sh -
-
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --disable traefik" sh -
