b7e92c842132ea1d5040acfc241241a809fd4ed0
iain <iain@rootisgod.com>
2024-02-21 21:17:01 +0000

    Try this method

--- kubernetes/prometheus-operator/install.sh
+++ kubernetes/prometheus-operator/install.sh
@@ -1,8 +1,9 @@
 # https://rpi4cluster.com/monitoring/k3s-prometheus-oper/
 # https://prometheus-operator.dev/docs/user-guides/getting-started/
+#LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
+#curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/bundle.yaml > bundle.yaml
+
 kubectl create namespace monitoring
-LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
-curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/bundle.yaml > bundle.yaml
+wget https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
 sed -i 's/namespace: default/namespace: monitoring/g' bundle.yaml
-#cat bundle.yaml |  grep 'namespace: ' bundle.yaml
 kubectl apply -f bundle.yaml
