60b09dfa2405a42b8cb021a781f5afd7451615f6
iain <iain@rootisgod.com>
2024-02-21 21:10:15 +0000

    Do the install

--- kubernetes/prometheus-operator/install.sh
+++ kubernetes/prometheus-operator/install.sh
@@ -1,4 +1,8 @@
+# https://rpi4cluster.com/monitoring/k3s-prometheus-oper/
+# https://prometheus-operator.dev/docs/user-guides/getting-started/
+
 LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
 curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/bundle.yaml > bundle.yaml
 sed -i 's/namespace: default/namespace: monitoring/g' bundle.yaml
-cat bundle.yaml |  grep 'namespace: ' bundle.yaml
+#cat bundle.yaml |  grep 'namespace: ' bundle.yaml
+kubectl apply -f bundle.yaml
