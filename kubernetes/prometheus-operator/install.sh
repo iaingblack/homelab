c14a3ee8c4cf7c15bda8bbdbeafd9b63dcad7611
iain <iain@rootisgod.com>
2024-02-21 21:12:54 +0000

    Forgot to add the namespace

--- kubernetes/prometheus-operator/install.sh
+++ kubernetes/prometheus-operator/install.sh
@@ -1,6 +1,6 @@
 # https://rpi4cluster.com/monitoring/k3s-prometheus-oper/
 # https://prometheus-operator.dev/docs/user-guides/getting-started/
-
+kubectl create namespace monitoring
 LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
 curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/bundle.yaml > bundle.yaml
 sed -i 's/namespace: default/namespace: monitoring/g' bundle.yaml
