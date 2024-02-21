ab24931d8bcf2725d279b743422e1659ddc83937
iain <iain@rootisgod.com>
2024-02-21 21:34:53 +0000

    Okay, needs --server-side.Should work now

--- kubernetes/prometheus-operator/install.sh
+++ kubernetes/prometheus-operator/install.sh
@@ -1,11 +1,13 @@
 # https://rpi4cluster.com/monitoring/k3s-prometheus-oper/
 # https://prometheus-operator.dev/docs/user-guides/getting-started/
 
-# Doesnt seem to work. Ah well
-#wget https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
-#sed -i 's/namespace: default/namespace: monitoring/g' bundle.yaml
+# Install Prometheus Operator in a Monitoring Namespace
+kubectl create namespace monitoring
+wget https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
+sed -i 's/namespace: default/namespace: monitoring/g' bundle.yaml
+kubectl apply --server-side -f bundle.yaml
 
 # Can delete via kubectl delete -f bundle.yaml
-LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
-curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/bundle.yaml > bundle.yaml
-kubectl apply -f bundle.yaml
+#LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
+#curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/bundle.yaml > bundle.yaml
+#kubectl apply -f bundle.yaml
